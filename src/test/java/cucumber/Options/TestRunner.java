d6ea69e047a8c4c670614e38ee4c53141d739b00
Nurfatin <nurfatin@mytestbits.com>
2024-03-03 15:14:40 +0800

    Update: Reports

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -1,12 +1,8 @@
 package cucumber.Options;
 
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
-@RunWith(Cucumber.class)
 @CucumberOptions(
 
 	    features = "src/test/java/features",
@@ -19,7 +15,7 @@ import io.cucumber.junit.CucumberOptions;
 	)
 
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
 	
 	
 
